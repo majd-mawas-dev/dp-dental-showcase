9ef029e78414d5b812048104114c20f04ea3b064
Majd Mawas <majosmws@gmail.com>
2026-08-10 09:56:45 +0300

    fix: deploy Vite app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/new-dp/",
+  base: "/dp-dental-showcase/",
   plugins: [react()],
 });
